d9475e423731c6f4612d9f5e528ea12ae2b021b7
bhamicus42 <archetypetheta@gmail.com>
2018-11-07 10:38:06 -0800

    Delete Test_Player2EB_CB.m

--- Test_Player2EB_CB.m
+++
@@ -1,5 +0,0 @@
-% filename:Player2EB_CB.m
-% Purpose:Allows user to enter name of player 2/computer
-% date: 11/6/18
-% Authors: Ben Hoffman, Kyle Rockwell, Emmy Nam, Andre Sanchez
-% Engr-6 Group Stomadoapod srjc
