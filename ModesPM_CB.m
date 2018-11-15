f8e84d859ccf3252b21dcc3b17fa73cd70ee12e3
KyleRockwell <44984539+KyleRockwell@users.noreply.github.com>
2018-11-15 13:36:34 -0800

    Changes text based on mode

--- ModesPM_CB.m
+++ ModesPM_CB.m
@@ -5,3 +5,15 @@
 % Engr-6 Group Stomadoapod srjc
 
 Mode = get(gcbo, 'value');
+if length(Mode)>1
+    close all
+else
+    if Mode>1
+    set(findobj('tag','Player2Text'),'string','Enter Computer Name')
+    set(findobj('tag','TogTurnRB'),'string','Computer Starts')
+
+    else
+    set(findobj('tag','Player2Text'),'string','Enter Player 2 Name')
+    set(findobj('tag','TogTurnRB'),'string','Player 2 Starts')
+end
+end
--- TitleInit.m
+++ TitleInit.m
@@ -3,7 +3,7 @@
 % date: 11/6/18
 % Authors: Ben Hoffman, Kyle Rockwell, Emmy Nam, Andre Sanchez
 % Engr-6 Group Stomadoapod srjc
-
+Mode=1;
 TitleHandle = gcf;
 
 if ~exist('GameState')
