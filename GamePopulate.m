b0a0f87a509967ac87af76ee8e6e2330c57760a0
EmmyNam <35051814+EmmyNam@users.noreply.github.com>
2018-11-19 15:28:59 -0800

    Update GamePopulate.m
    Comments

--- GamePopulate.m
+++ GamePopulate.m
@@ -1,10 +1,12 @@
 % filename:  GamePopulate.m
-% Purpose:   Populates game board and (SCORES?) if they exist
+% Purpose:   Populates game board and (SCORES?) if player leaves to help, main menu, or credits
 % date:      11/19/2018
 % Authors:   Ben Hoffman, Kyle Rockwell, Emmy Nam, Andre Sanchez
 % Engr-6 Group Stomadoapod srjc
 
-%populates game board if player leaves to help, main menu, or credits
+% last edited by Emmy Nam 11/19/2018 15:25
+
+
 if exist('GameState')
     for BoxIndex = 1:9
         if GameState(BoxIndex) == 1
