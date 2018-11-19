2ac7b14d0808cee9ee03f0dc1752105340f9a92e
KyleRockwell <44984539+KyleRockwell@users.noreply.github.com>
2018-11-19 15:00:43 -0800

    oops sorry emmy, undoing overwrite

--- ModesPM_CB.m
+++ ModesPM_CB.m
@@ -1,9 +1,18 @@
 
+
 % filename:ModesPM_CB.m
 % Purpose:Allows select gameplay mode
 % date: 11/6/18
-% Authors: Kyle Rockwell
+% Authors: Ben Hoffman, Kyle Rockwell, Emmy Nam, Andre Sanchez
 % Engr-6 Group Stomadoapod srjc
+
+%forces players to start new game if mode is changed
+%Note: to ensure mode is correct if navigate to main menu, reset mode=1 in TitlePB_CB.m
+%when go to title screen because human v. human is auto filled by PM 
+if Mode ~= get(gcbo, 'value')
+    clear GameState
+    GameSpaceVarInit
+end
 
 Mode = get(gcbo, 'value');
 if length(Mode)>1
@@ -14,7 +23,7 @@ else
     set(findobj('tag','TogTurnRB'),'string','Computer Starts')
 
     else
-    set(findobj('tag','Player2Text'),'string','Enter Player two Name')
-    set(findobj('tag','TogTurnRB'),'string','Player two Starts')
+    set(findobj('tag','Player2Text'),'string','Enter Player 2 Name')
+    set(findobj('tag','TogTurnRB'),'string','Player 2 Starts')
 end
 end
