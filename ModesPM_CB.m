be3347823526939f34db29d6f6e84c9b4c3f5113
KyleRockwell <44984539+KyleRockwell@users.noreply.github.com>
2018-11-19 14:13:12 -0800

    Add files via upload

--- ModesPM_CB.m
+++ ModesPM_CB.m
@@ -1,3 +1,4 @@
+
 % filename:ModesPM_CB.m
 % Purpose:Allows select gameplay mode
 % date: 11/6/18
@@ -5,3 +6,15 @@
 % Engr-6 Group Stomadoapod srjc
 
 Mode = get(gcbo, 'value');
+if length(Mode)>1
+    close all
+else
+    if Mode>1
+    set(findobj('tag','Player2Text'),'string','Enter Computer Name')
+    set(findobj('tag','TogTurnRB'),'string','Computer Starts')
+
+    else
+    set(findobj('tag','Player2Text'),'string','Enter Player 2 Name')
+    set(findobj('tag','TogTurnRB'),'string','Player 2 Starts')
+end
+end
