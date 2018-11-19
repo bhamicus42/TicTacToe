f7ddd81e8e44df3d474f8aee404eb989c22ab27f
andre-sanchez <44880029+andre-sanchez@users.noreply.github.com>
2018-11-19 14:45:21 -0800

    Add files via upload

--- BoxCB.m
+++ BoxCB.m
@@ -1,38 +1,89 @@
 % filename:BoxCB
-% Purpose:toggles image on box to show playerâ€™s current move
+% Purpose:toggles image on box to show player’s current move
 % date: 11/6/18
 % Authors: Ben Hoffman, Kyle Rockwell, Emmy Nam, Andre Sanchez
 % Engr-6 Group Stomadoapod srjc
 
+% BoxNum = get num from tag;
+% set(gcbo, UserData, WhoseTurn);
+% GameState(BoxNum) = WhoseTurn;
+% 
+% if WhoseTurn>0
+%     set(gcbo, string, 'X');
+% else
+%     set(gcbo, string, 'O');
+% end 
+% 
+% check for win
+%     if abs sum of GameState along any direction, win
+%     to sum up diagonally, chek this:  https://www.mathworks.com/matlabcentral/answers/245419-how-to-sum-matrix-diagonals
+%      check trace
+% 
+% check for tie
+%     if no GameState 0, tie
+%         
+% if difficulty > 0
+%     computer choose next move
+%     options = find(~GameState)
+
+%setPlayerInfo based on whoseTurn
+if WhoseTurn > 0
+    Player = 1;
+    Opponent = 2;
+    PlayerSymbol = 'X';
+elseif WhoseTurn < 0
+    Player = 2;
+    Opponent = 1;
+    PlayerSymbol = 'O';
+end
+
+%Emmy, were you intending to change this to a findobj?  -Ben
 BoxTag = (get(gcbo, 'tag'));
 BoxNum = str2num(BoxTag(end));
 
-%Everything inside this if statement only executes if the box isn't
-%already occupied
-if GameState(BoxNum) == 0
-    SetPlayerData
+GameState(BoxNum) = WhoseTurn;
+set(gcbo, 'string', PlayerSymbol);
 
-    %gets the current box and sets its string to the right symbol
-    GameState(BoxNum) = WhoseTurn;
-    set(gcbo, 'string', PlayerSymbol);
-
-    CheckForEndGame
-
-    %COMPUTER TURN
-    WhoseTurn = -WhoseTurn; %1 or -1 to indicated player turn init in TicTacInit
-    
-    if (Mode > 1) & (EndGame ~= 1)
-        SetPlayerData
-        PossibleMoves = find(~GameState);
-
-        switch Mode
-            case 2
-                ChooseNextEasy
-            case 3
-    %             ChooseNextModerate
-            case 4
-    %             ChooseNextExpert
-        end
-    WhoseTurn = -WhoseTurn;    
-    end
+%Checks for possible wins
+for Index = 1:length(PossWins)
+    CheckWins = [CheckWins, sum(GameState(PossWins(Index,:)))]
 end
+
+if max(CheckWins) == 3
+    EndGame = 1;
+else
+    CheckWins = [];
+end
+
+%checks if game should be over
+if (sum(sum(abs(GameState)))) == 9
+    Ties = Ties + 1
+    TieBool = 1;
+    EndGame = 1;
+end
+ 
+if EndGame
+    set(findobj('tag', 'GameOverPB'), 'Visible', 'on')
+    set(findobj('tag', 'GameOverPB'), 'Enable', 'on')
+end
+
+%COMPUTER TURN
+% WhoseTurn = 1
+% Player = 'Player1'
+% PlayerNum = str2num(Player(end))
+% Player = ['Player' , num2str(PlayerNum + WhoseTurn)]  %Not right eq
+WhoseTurn = -WhoseTurn; %1 or -1 to indicated player turn init in TicTacInit
+
+% 
+% 
+% 
+% 
+
+
+set(findobj('tag',Player1Text),'string',Player1);
+set(findobj('tag',Player2Text),'string',Player2);
+set(findobj('tag',P1Wins),'string',num2str(PlayerWins(1)));
+set(findobj('tag',P2Wins),'string',num2str(PlayerWins(2)));
+set(findobj('tag',Ties),'string',num2str(Ties));
+set(findobj('tag',P1Losses),'string',num2str(PlayerLosses(1)));
+set(findobj('tag',P2Losses),'string',num2str(PlayerLosses(2)));
