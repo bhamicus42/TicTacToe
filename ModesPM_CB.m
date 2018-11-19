59073c8299fff7d8a4e6f283285f5cbf7408e28e
KyleRockwell <44984539+KyleRockwell@users.noreply.github.com>
2018-11-19 14:55:58 -0800

    Kyle-sound& text changes
    added music and minor text changes

--- ModesPM_CB.m
+++ ModesPM_CB.m
@@ -2,16 +2,8 @@
 % filename:ModesPM_CB.m
 % Purpose:Allows select gameplay mode
 % date: 11/6/18
-% Authors: Ben Hoffman, Kyle Rockwell, Emmy Nam, Andre Sanchez
+% Authors: Kyle Rockwell
 % Engr-6 Group Stomadoapod srjc
-
-%forces players to start new game if mode is changed
-%Note: to ensure mode is correct if navigate to main menu, reset mode=1 in TitlePB_CB.m
-%when go to title screen because human v. human is auto filled by PM 
-if Mode ~= get(gcbo, 'value')
-    clear GameState
-    GameSpaceVarInit
-end
 
 Mode = get(gcbo, 'value');
 if length(Mode)>1
@@ -22,7 +14,7 @@ else
     set(findobj('tag','TogTurnRB'),'string','Computer Starts')
 
     else
-    set(findobj('tag','Player2Text'),'string','Enter Player 2 Name')
-    set(findobj('tag','TogTurnRB'),'string','Player 2 Starts')
+    set(findobj('tag','Player2Text'),'string','Enter Player two Name')
+    set(findobj('tag','TogTurnRB'),'string','Player two Starts')
 end
 end
