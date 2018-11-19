75a7a563c343d0a3da47bfd4eb7e6a68088722c8
EmmyNam <35051814+EmmyNam@users.noreply.github.com>
2018-11-19 15:12:30 -0800

    Add GamePopulate.m file
    New file for populating game board (previously existed in GameSpaceVarInit)

---
+++ GamePopulate.m
@@ -0,0 +1,19 @@
+% filename:  GamePopulate.m
+% Purpose:   Populates game board and (SCORES?) if they exist
+% date:      11/19/2018
+% Authors:   Ben Hoffman, Kyle Rockwell, Emmy Nam, Andre Sanchez
+% Engr-6 Group Stomadoapod srjc
+
+%populates game board if player leaves to help, main menu, or credits
+if exist('GameState')
+    for BoxIndex = 1:9
+        if GameState(BoxIndex) == 1
+            set(findobj('tag', ['Box', num2str(BoxIndex)]), 'string', 'X')
+        elseif GameState(BoxIndex) == -1
+            set(findobj('tag', ['Box', num2str(BoxIndex)]), 'string', 'O')
+        else 
+            set(findobj('tag', ['Box', num2str(BoxIndex)]), 'string', '')
+        end
+    end
+    CheckWins = [];
+end
