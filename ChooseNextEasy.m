06d66965369598088ac3a399a3f43d0591e4b3ae
Ben Hoffman <archetypetheta@gmail.com>
2018-11-21 11:48:48 -0800

    Update ChooseNextEasy.m

--- ChooseNextEasy.m
+++ ChooseNextEasy.m
@@ -1,13 +1,9 @@
-% filename: ChooseNextEasy.m
-% purpose:  Chooses an available box at random and marks it
-% date:     11/6/18
-% authors:  Ben Hoffman
+% filename:  ChooseNextEasy.m
+% purpose:   Chooses an available box at random and marks it
+% date:      11/6/18
+% authors:   Ben Hoffman
 % 
 % Engr-6 Group Stomadoapod srjc
 
-% generate random int from 1 to length of PossibleMoves\
-% chosen move is PossibleMoves(rand)
-
 CompMoveChoice = PossibleMoves(randi(length(PossibleMoves)));
-set(findobj('tag', ['Box', num2str(CompMoveChoice)]), 'string', PlayerSymbol)
-GameState(CompMoveChoice) = WhoseTurn;
+MarkBox
