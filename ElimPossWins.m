306694d0f572196ebb90217489b55016d69bc9a3
KyleRockwell <44984539+KyleRockwell@users.noreply.github.com>
2018-11-29 13:21:23 -0800

    translated ELimPossWins Kyle&Em
    the gamestate is transposed

--- ElimPossWins.m
+++ ElimPossWins.m
@@ -1,17 +1,54 @@
-%filename: ElimPossWins.m
-%purpose: gets rid of PossWins that have player move already in a space
-%author: Emmy Nam
+% %filename: ElimPossWins.m
+% %purpose: gets rid of PossWins that have player move already in a space and use new array to find computer’s best option
+% %author: Emmy Nam
 
+PossWins = [1 2 3; 4 5 6; 7 8 9; 
+            1 5 9; 3 5 7; 
+            1 4 7; 2 5 8; 3 6 9];
 
-NewPossWins = []
-for options = 1:length(PossWins)
-    PossWinOpt = PossWins(options, :)
-    for box = 1:3
-        if GameState(box) == 1
-            PossWinOpt = [];
-            disp('Player here: ' + num2str(box))
-        end
-    end
-    NewPossWins = [NewPossWins; PossWinOpt]
+GameState =   [-1 1 0; 0 0 0; 0 0 0];
+
+FinalPossWins = [];
+CompPossWins = [];
+EmptyPossWins = [];
+
+for Options = 1:length(PossWins)
+	PossWinOpt = PossWins(Options, :); %pulls one win option
+    disp(['Poss wins opt: ', num2str(PossWinOpt)])
+   	Count = 0 ; %init Count and IsEmpty for each poss win 
+    	IsEmpty = 1;
+
+    	for Index = 1:3
+        	BoxNum = PossWinOpt(Index);
+	     	if GameState(BoxNum) ~= 0
+            	IsEmpty = 0 ;
+            end
+        	Count = Count + GameState(BoxNum);
+		end
+end	
+    
+    %After check all boxes in current PossWin Option
+
+if IsEmpty
+    EmptyPossWins = [EmptyPossWins, (PossWinOpt)];
+elseif Count < 0
+    CompPossWins = [CompPossWins, (PossWinOpt)];
 end
+    
+
+
+if length(CompPossWins) > 0
+    FinalPossWins = CompPossWins;
+elseif length(EmptyPossWins) > 0
+    FinalPossWins = EmptyPossWins;
+else
+    disp('No good options')
+end
+%RANDOMLY CHOOSE SPOT (Will do through ChooseNextExpert)
+
+FinalPossWins %displays the new array (used for debugging)
+
+%NEXT TASK: use this array to randomly choose box that is not occupied
+%Look at CompChoiceMove in ModerateAI to see if this is being done (note: be careful because the array will change sizes)
+
 
