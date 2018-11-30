cfe0b30fdd7b62b523998c0b12f2bfa17a152af9
KyleRockwell <44984539+KyleRockwell@users.noreply.github.com>
2018-11-29 17:36:54 -0800

    Works now - Em
    Returns matrix of only the best options for winning (includes boxes that are already used)

--- ElimPossWins.m
+++ ElimPossWins.m
@@ -2,41 +2,44 @@
 % %purpose: gets rid of PossWins that have player move already in a space and use new array to find computer’s best option
 % %author: Emmy Nam
 
-PossWins = [1 2 3; 4 5 6; 7 8 9; 
-            1 5 9; 3 5 7; 
-            1 4 7; 2 5 8; 3 6 9];
-
-GameState =   [-1 1 0; 0 0 0; 0 0 0];
+%Used Var below to test file
+% PossWins = [1 2 3; 4 5 6; 7 8 9; 
+%         1 5 9; 3 5 7; 
+%         1 4 7; 2 5 8; 3 6 9];
+% 
+% GameState =   [1,0, 0; ...
+%             0 0 0; ...
+%             0 0 0];
 
 FinalPossWins = [];
 CompPossWins = [];
 EmptyPossWins = [];
 
 for Options = 1:length(PossWins)
-	PossWinOpt = PossWins(Options, :); %pulls one win option
+    PossWinOpt = PossWins(Options, :); %pulls one win option
     disp(['Poss wins opt: ', num2str(PossWinOpt)])
    	Count = 0 ; %init Count and IsEmpty for each poss win 
-    	IsEmpty = 1;
+    IsEmpty = 1;
 
-    	for Index = 1:3
-        	BoxNum = PossWinOpt(Index);
-	     	if GameState(BoxNum) ~= 0
-            	IsEmpty = 0 ;
-            end
-        	Count = Count + GameState(BoxNum);
-		end
+	for Index = 1:3
+        BoxNum = PossWinOpt(Index);
+        if GameState(BoxNum) ~= 0
+            IsEmpty = 0 ;
+        end
+        Count = Count + GameState(BoxNum);
+    end
+
+    %Append possWinOpt to correct category
+    if IsEmpty
+        EmptyPossWins = [EmptyPossWins; PossWinOpt];
+    elseif Count < 0
+        CompPossWins = [CompPossWins; PossWinOpt];
+    end
+
 end	
     
-    %After check all boxes in current PossWin Option
 
-if IsEmpty
-    EmptyPossWins = [EmptyPossWins, (PossWinOpt)];
-elseif Count < 0
-    CompPossWins = [CompPossWins, (PossWinOpt)];
-end
-    
-
-
+%After check all boxes in current PossWin Option
 if length(CompPossWins) > 0
     FinalPossWins = CompPossWins;
 elseif length(EmptyPossWins) > 0
